9066bd38af6b748b384eaad4b9911d66df4e26e0
xujunmeng.github <xujunmeng2012@163.com>
2020-01-21 17:54:55 +0800

    update

--- src/main/java/com/company/project/MainApplication.java
+++ src/main/java/com/company/project/MainApplication.java
@@ -1,9 +1,13 @@
 package com.company.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(value = "mybatis.mapper")
+@EnableTransactionManagement(proxyTargetClass = true)
 public class MainApplication {
 
     public static void main(String[] args) {
