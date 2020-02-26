cb654f0072a86f11de499c52dec3835531b81fb0
xujunmeng.github <xujunmeng2012@163.com>
2020-02-26 11:18:16 +0800

    update

--- src/main/java/com/company/project/MainApplication.java
+++ src/main/java/com/company/project/MainApplication.java
@@ -3,15 +3,11 @@ package com.company.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan(value = "com.company.project.biz.mapper")
 @EnableTransactionManagement(proxyTargetClass = true)
-@EnableAspectJAutoProxy(exposeProxy = true)
-@EnableAsync(proxyTargetClass = true)
 public class MainApplication {
 
     public static void main(String[] args) {
